995171fa7f9630131ca51f731c3f3c0f3d15b2f5
Carl Inniss <carlinniss@gmail.com>
2026-09-02 16:19:04 -0400

    Set Docker local environment defaults

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -13,6 +13,34 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
+set_env() {
+    key="$1"
+    value="$2"
+
+    if grep -q "^${key}=" .env; then
+        sed -i "s#^${key}=.*#${key}=${value}#" .env
+    else
+        printf '\n%s=%s\n' "$key" "$value" >> .env
+    fi
+}
+
+set_env APP_ENV local
+set_env APP_DEBUG true
+set_env DB_CONNECTION mysql
+set_env DB_HOST db
+set_env DB_PORT 3306
+set_env DB_DATABASE openclassify
+set_env DB_USERNAME openclassify
+set_env DB_PASSWORD secret
+set_env CACHE_STORE database
+set_env SESSION_DRIVER database
+set_env QUEUE_CONNECTION database
+set_env BROADCAST_CONNECTION null
+set_env FILESYSTEM_DISK public
+set_env MEDIA_DISK public
+set_env LOCAL_MEDIA_DISK public
+set_env LIVEWIRE_TEMPORARY_FILE_UPLOAD_DISK local
+
 composer install
 npm install
 
